7247ed706a941a34757171955714e88b8d615320
j-scull <joe-scullion@hotmail.com>
2021-06-22 21:07:15 +0100

    comments for email and click

--- app/email.py
+++ app/email.py
@@ -1,29 +1,54 @@
+"""
+Provides email support for the blog.
+Cases where user's are emailed by the blog include:
+- password reset requests
+- user post download requests
+
+An email server can be set up locally for test purposes using:
+>> python -m smtpd -n -c DebuggingServer localhost:8025
+>> export MAIL_SERVER=localhost
+>> export MAIL_PORT=8025
+
+To set up a gmail server do the following:
+>> export MAIL_SERVER=stmp.googlemail.com
+>> export MAIL_PORT=587
+>> export MAIL_USE_TLS=1
+>> export MAIL_USERNAME=<gmail address>
+>> export MAIL_PASSWORD=<password>
+
+"""
+
 from threading import Thread
 from flask_mail import Message
 from app import mail
 from flask import current_app
 
 
-# set up local email server
-# python -m smtpd -n -c DebuggingServer localhost:8025
-# set MAIL_SERVER=localhost
-# set MAIL_PORT=8025
-
-# set up gmail server
-# set MAIL_SERVER=stmp.googlemail.com
-# set MAIL_PORT=587
-# set MAIL_USE_TLS=1
-# set MAIL_USERNAME=<gmail address>
-# set MAIL_PASSWORD=<password>
-
 
 
 def send_async_email(app, msg):
+    """
+    Sends an email
+    --------------
+    Parameters:
+    app - the app
+    msg - the Message object to be sent
+    """
     with app.app_context():
         mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
+    """
+    Sends an email - sets up a thread to send the email asychronously
+    -----------------------------------------------------------------
+    Parameters:
+    subject - the email subject
+    sender - the sender of the email
+    recipients - the recipients of the email
+    text_body - the email content in txt
+    html_body - the email content in html
+    """
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
