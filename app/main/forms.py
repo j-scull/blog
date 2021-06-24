eabcabfef4af6ceb1b83ba9acd84a64bd6cd02bc
j-scull <joe-scullion@hotmail.com>
2021-06-24 11:54:29 +0100

    Adding Documentation

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,8 @@
+"""
+Provides forms for the main functionality of the blog,
+i.e. editing profiles, posting, messaging and search
+"""
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Length
@@ -7,6 +12,10 @@ from flask import request
 
 
 class EditProfileForm(FlaskForm):
+    """
+    Allows a user to edit their profile.
+    Ensures there are no duplicaes usernames.
+    """
     username = StringField(_l('Username'), validators=[DataRequired()])
     about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
     submit = SubmitField(_l('Submit'))
@@ -22,17 +31,26 @@ class EditProfileForm(FlaskForm):
                 raise ValidationError(_l('Please use a different name.'))
 
 
+
 # Invisible Form for following other users
 class EmptyForm(FlaskForm):
     submit = SubmitField(_l('Submit'))
 
 
+
 class PostForm(FlaskForm):
+    """
+    Form for creating posts.
+    """
     post = TextAreaField(_l('Say something'), validators=[DataRequired(), Length(min=1, max=140)])
     submit = SubmitField(_l('Submit'))
 
 
+
 class SearchForm(FlaskForm):
+    """
+    Form for searching posts.
+    """
     q = StringField(_l('Search'), validators=[DataRequired()])
     
     def __init__(self, *args, **kwargs):
@@ -43,7 +61,11 @@ class SearchForm(FlaskForm):
         super(SearchForm, self).__init__(*args, **kwargs)
 
 
+
 class MessageForm(FlaskForm):
+    """
+    Form for sending private messages to other users.
+    """
     message = TextAreaField(_l('Message'), validators=[
         DataRequired(), Length(min=0, max=140)])
     submit = SubmitField(_l('Submit'))
