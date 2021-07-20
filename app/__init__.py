90b169d222c09f9d08df53a0ba7d0d951c6c4834
j-scull <joe-scullion@hotmail.com>
2021-07-20 17:54:14 +0100

    setting up api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -47,6 +47,8 @@ def create_app(config_class=Config):
     app.register_blueprint(auth_bp, url_prefix='/auth')
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
