ee40aac7dcdb4ca49f5a245e12150c5e5c86a364
j-scull <joe-scullion@hotmail.com>
2021-08-12 15:43:20 +0100

    API token authentication

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from app import db
 from app.errors import bp
+from app.api.errors import error_response as api_error_response
 
 
 @bp.errorhandler(404)
@@ -11,4 +12,25 @@ def not_found_error(error):
 @bp.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
+    return render_template('errors/500.html'), 500
+
+
+# Below are API specific error handlers.
+# These use content negotiation to determine what format to send to the client.
+
+def wants_json_response():
+    return request.accept_mimetypes['application/json'] >= \
+        request.accept_mimetypes['text/html']
+
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    if wants_json_response():
+        return api_error_response(404)
+    return render_template('errors/404.html'), 404
+
+@bp.app_errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    if wants_json_response():
+        return api_error_response(500)
     return render_template('errors/500.html'), 500
--- app/models.py
+++ app/models.py
@@ -5,14 +5,15 @@ Defines the database models for the blog
 from hashlib import md5
 from app import login, db
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import time
 from flask_login import UserMixin
 import jwt
 from flask import current_app, url_for
 from app.search import add_to_index, remove_from_index, query_index
 import json
-
+import base64
+import os
 
 # Create followers table - seconadary association table used in User class
 followers = db.Table('followers', 
@@ -70,7 +71,9 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     # set up many-to-many follower-followed relationship
     followed = db.relationship(
         'User', 
@@ -228,6 +231,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 
 
